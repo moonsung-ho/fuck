6eb9650234c665382bf0419e4b2d437c594ffbae
moonsung-ho <sungho.moon@aol.com>
2024-06-04 00:27:26 +0900

    chore: Update background size and project links in App.css and App.js

--- src/App.js
+++ src/App.js
@@ -59,22 +59,39 @@ function App() {
       </section>
       <section id="first" className="px-7 md:px-10 pt-6 mb-52">
         <div>
-          <p className="text-white text-4xl font-p font-bold">About</p>
+          <p className="text-white text-4xl font-p font-bold">Me</p>
           <p className="text-white text-lg md:text-xl font-pretendard font-regular mt-6">
-            학생 문성호입니다.
+            <a href="https://yonsin.sen.ms.kr" className="text-blue-400">
+              연신중학교
+            </a>
+            에 다니는학생 문성호입니다.
             <br />
-            주로 쓸데없지만 있으면 도움이 되는 것들을 만듭니다.
+            주로{" "}
+            <button
+              className="text-blue-400"
+              onClick={() => lenis.scrollTo("section#second")}
+            >
+              쓸데없지만 있으면 좋은 것들
+            </button>
+            을 만듭니다.
             <br />
+            여러 가지 잡다한 것들에 관심이 많습니다.
+            <br />
+            지금은{" "}
+            <a href="https://clovertimes.xyz" className="text-blue-400">
+              신문동아리
+            </a>
+            에서 신문 제작에 몰두하고 있습니다.
           </p>
         </div>
       </section>
       <hr className="between12 mx-6 md:mx-9 mt-20"></hr>
       <section id="second" className="px-7 md:px-10 pt-6">
         <p className="text-white text-4xl font-p font-bold">Projects</p>
-        <div className="horizontalparent">
+        {/* <div className="horizontalparent">
           <a href="https://ybs.sungho.xyz">
-            <div className="horizontal flex mb-20 mt-6 h-52 md:h-[30rem] overflow-hidden overflow-x-hidden overflow-y-hidden w-64 md:w-fit sticky top-0">
-              <div className="project ybs w-[45rem] aspect-4/3 h-full pl-6 relative">
+            <div className="horizontal mb-20 mt-6 h-52 md:h-[30rem] w-[10px] md:w-fit sticky top-0">
+              <div className="project h-full pl-6 relative">
                 <p className="text-black text-2xl md:text-2xl font-pretendard font-bold mt-6">
                   연신중 방송부
                 </p>
@@ -108,6 +125,42 @@ function App() {
                 </p>
               </div>
             </div>
+          </a>
+        </div> */}
+        <div className="mt-6 mb-20 flex flex-row overflow-x-auto  whitespace-nowrap">
+          <a
+            href="https://ybs.sungho.xyz"
+            className="project ybs w-64 md:w-[600px] h-52  md:min-w-[600px] md:h-[30rem] inline-flex flex-col pb-5 pr-6 pl-5 justify-between"
+          >
+            <p className="text-black text-2xl md:text-2xl font-pretendard font-bold mt-6">
+              연신중 방송부
+            </p>
+            <p className="text-black md:text-lg font-pretendard overflow-hidden font-medium mt-6 ">
+              교내 방송부 활동을 하며 신청곡과 사연을 받는 웹사이트를 직접
+              개발했습니다.
+            </p>
+          </a>
+          <a
+            href="https://clovertimes.xyz"
+            className="project newspaper w-64 md:w-[600px] ml-5 md:min-w-[600px] h-52 md:h-[30rem] inline-flex pr-6 flex-col pb-5 pl-5 justify-between"
+          >
+            <p className="text-black text-2xl md:text-2xl font-pretendard font-bold mt-6">
+              토끼풀 타임즈
+            </p>
+            <p className="text-black md:text-lg overflow-hidden font-pretendard font-medium mt-6 ">
+              학교에서 신문부를 창설하여 발행인을 맡고 있습니다.
+            </p>
+          </a>
+          <a
+            href="https://geupsiktime.netlify.app"
+            className="project mealtime w-64 md:w-[45rem] ml-5 h-52 md:min-w-[600px] md:h-[30rem] inline-flex pr-6 flex-col pb-5 pl-5 justify-between"
+          >
+            <p className="text-white text-2xl md:text-2xl font-pretendard font-bold mt-6">
+              급식시간
+            </p>
+            <p className="text-white md:text-lg font-pretendard overflow-hidden font-medium mt-6 ">
+              학교 급식과 시간표를 알려주는 앱을 개발했습니다.
+            </p>
           </a>
         </div>
       </section>
